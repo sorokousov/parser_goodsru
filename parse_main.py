e4a8aafedbb9f7e550ff51704288e185cb670e37
sorok <qwedqw>
2020-12-16 13:31:32 +0100

    Save

--- parse_main.py
+++ parse_main.py
@@ -71,34 +71,32 @@ def main():
         # print(product_['sku'])
         # MySQL().set_product_main_in_process(id_product=product_['id'])
 
-        try:
+        if product_:
             response = get_soup(url=product_['link_product'])
-        except:
-            break
-        sku = product_['sku']
-        id_product = product_['id']
-        id_category = product_['id_category']
+            sku = product_['sku']
+            id_product = product_['id']
+            id_category = product_['id_category']
 
-        description = get_description(response=response)
-        # images = get_images(response=response)
+            description = get_description(response=response)
+            # images = get_images(response=response)
 
-        result = True
-        ftp_result = None
-        # if images:
-        #     ftp_result = save_to_disk(files=images, sku=sku, id_category=id_category)
-        # if images is False:
-        #     result = False
+            result = True
+            ftp_result = None
+            # if images:
+            #     ftp_result = save_to_disk(files=images, sku=sku, id_category=id_category)
+            # if images is False:
+            #     result = False
 
-        if result and description:
-            # print('IF')
-            if ftp_result:
-                MySQL().write_images(images=ftp_result)
-            MySQL().write_description(sku=sku, description=description)
-            MySQL().set_product_main_ready(id_product=id_product)
-        else:
-            # print('ELSE')
-            MySQL().set_product_main_ready(id_product=id_product)
-            # MySQL().set_product_main_bad(id_product=id_product)
+            if result and description:
+                # print('IF')
+                if ftp_result:
+                    MySQL().write_images(images=ftp_result)
+                MySQL().write_description(sku=sku, description=description)
+                MySQL().set_product_main_ready(id_product=id_product)
+            else:
+                # print('ELSE')
+                MySQL().set_product_main_ready(id_product=id_product)
+                # MySQL().set_product_main_bad(id_product=id_product)
 
         n += 1
         print('\rCount', n, 'Time', datetime.now() - start_time, end='')
